5e064fce08ed7d4640f21fa9b6a5b97fd19946ec
ddavisgraphics <dave@ddavisgraphics.com>
2018-12-04 14:22:56 -0500

    Admin Views and Interfaces.

--- app/assets/javascripts/plugins/search_filter.js
+++ app/assets/javascripts/plugins/search_filter.js
@@ -1,28 +1,41 @@
 $( document ).on('turbolinks:load', function() {
   if($('#search_filter').length){
     $('#search_filter').keyup(function(e) { 
-      var search_terms = $(this).val().toLowerCase().split(' '); 
+      var searchTerm = $(this).val().toLowerCase(); 
       $('.filter-list').each(function(){
-        var keywords = $(this).find('.keywords').text().toLowerCase().split(' '); 
-        if(search(search_terms, keywords)){ 
-          $(this).show(); 
-        } else {
+        var searchText = $(this).find('.keywords').text().replace(/\s/g,'').toLowerCase();
+        if(searchText.indexOf(searchTerm) === -1){
           $(this).hide(); 
+        } else { 
+          $(this).show(); 
         }
-      }); 
+      });
     });
   } 
 });
 
-function search(search_terms, search_text) {
-  for (var i = 0; i < search_text.length; i++) {
-    var text =  search_text[i].innerHTML.toLowerCase();
-    for(var f = 0; f < search_terms.length; f++) {
-      if (text.indexOf(search_terms[f]) > -1 ) {    
-        return true;
-      } else {
-        return false; 
-      }
-    }
-  }
-}
+// function search(elm, search_terms, search_text) {
+
+//   for (var i = 0; i < search_text.length; i++) {
+//     var text =  search_text[i];
+//     for(var f = 0; f < search_terms.length; f++) {
+//       console.log("search result:" + text.indexOf(search_terms[f]))
+//       if (text.indexOf(search_terms[f]) > -1 ) {    
+//         elm.show(); 
+//       } else {
+//         elm.hide();  
+//       }
+//     }
+//   }
+// }
+
+// function search(elm, search_terms, search_text){ 
+//   $('.employee-list').each(function(){
+//     var searchText = $(this).find('.keywords').text().toLowerCase();
+//     if(searchText.indexOf(searchTerm) === -1){
+//       $(this).hide(); 
+//     } else { 
+//       $(this).show(); 
+//     }
+//   });
+// }
