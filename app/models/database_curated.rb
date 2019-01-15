fe20dcb1c875de920e64b157348e1ec88991e5fc
ddavisgraphics <dave@ddavisgraphics.com>
2019-01-15 10:37:08 -0500

    Enumeration and Helper functions for pulling subject and database name in odd cases.

--- app/models/database_curated.rb
+++ app/models/database_curated.rb
@@ -5,4 +5,25 @@ class DatabaseCurated < ApplicationRecord
   # associations
   belongs_to :database
   belongs_to :subject
+
+  # sorting
+  enum sort: { 'High' => 3, 
+               'Medium' => 2,  
+               'Low' => 1,
+               'Default' => 0
+             }
+
+  # Returns the Subjects Name unless the item is nil
+  # @author David J. Davis
+  # @return string
+  def subject_name 
+    subject.name unless subject.nil?
+  end
+  
+  # Returns the Databses name unles the itme is nil
+  # @author David J. Davis
+  # @return string
+  def database_name 
+    database.name unless database.nil?
+  end 
 end
