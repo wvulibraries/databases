0dbd3eef8471ddffe236e60a98c2f619d2439493
ddavisgraphics <dave@ddavisgraphics.com>
2019-02-27 14:43:53 -0500

    Try this for travis.

--- spec/support/elastic_search.rb
+++ spec/support/elastic_search.rb
@@ -1,6 +1,12 @@
 RSpec.configure do |config|
   config.before(:each) do
-    Database.__elasticsearch__.create_index!
+    begin
+      Database.__elasticsearch__.create_index!
+      Database.__elasticsearch__.refresh_index!
+    rescue Elasticsearch::Transport::Transport::Errors::NotFound => e
+    rescue => e
+      STDERR.puts "There was an error creating the elasticsearch index for #{model.name}: #{e.inspect}"
+    end
   end
 
   config.after(:each) do
