8b9302d76b5d10cbeddf45950be4c7e8900f81fd
ddavisgraphics <dave@ddavisgraphics.com>
2019-05-02 19:26:26 -0400

    conversion script.

--- scripts/convert_descriptions.rb
+++ scripts/convert_descriptions.rb
@@ -1,6 +1,6 @@
 databases = Database.all
 databases.each do |db|
   description = CGI.unescapeHTML(db.description).strip
-  db.description = description 
+  db.description = description
   db.save(validate: false)
 end
