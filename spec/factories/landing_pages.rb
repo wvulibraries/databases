e755015fe03df3414d14897ff2c137c7f980b4cc
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2019-07-24 15:34:02 -0400

    updated faker
    factories needed to be modified they added the prefix Movies and TvShows so the tests were failing due to this change.

--- spec/factories/landing_pages.rb
+++ spec/factories/landing_pages.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :landing_page do
-    instructions { Faker::GameOfThrones.quote }
-    contact_name { Faker::GameOfThrones.character }
+    instructions { Faker::TvShows::GameOfThrones.quote }
+    contact_name { Faker::TvShows::GameOfThrones.character }
     contact_email { Faker::Internet.free_email }
     contact_phone_number { "(304) 293-xxxx"  } 
     contact_title { "Science Librarian"}
