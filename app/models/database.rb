31b129642b1269d14cf8d92e4126a83ccb6c8724
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2020-02-18 09:35:06 -0500

    Keyword search fix (#30)
    * Revert "Added New Databases to right navigation"
    
    This reverts commit 7ed183a46de2d5ce7ea595a0fc82a4c1af585b19.
    
    * added keywords to database.rb
    
    adjusted database model to use keywords during searching.
    
    * Search bar fix
    
    Search bar needed to be included inside the else statement in the view.
    
    * updated searchable and test
    
    updated searchable with lambda's. Removed the update method for elasticsearch.

--- app/models/database.rb
+++ app/models/database.rb
@@ -272,7 +272,6 @@ class Database < ApplicationRecord
       thumbnail: '',
       content_id: self.libguides_id
     }
-
   end
 
   # Elastic search settings using indexed json. 
@@ -280,8 +279,8 @@ class Database < ApplicationRecord
   # rake elasticsearch:import:model CLASS='Database' SCOPE="production" FORCE=y
   def as_indexed_json(_options)
     as_json(
-      methods: [:vendor_name, :subject_search_index, :rss_search_index, :curated_search_index, :published],
-      only: [:id, :name, :vendor_name, :description, :title_search]
+      methods: [:vendor_name, :subject_search_index, :rss_search_index, :curated_search_index, :published, :keywords],
+      only: [:id, :name, :vendor_name, :description, :title_search, :keywords]
     )
   end
 
@@ -294,6 +293,7 @@ class Database < ApplicationRecord
     indexes :subject_search_index, type: :text, analyzer: 'english'
     indexes :rss_search_index, type: :text, analyzer: 'english'
     indexes :curated_search_index, type: :text, analyzer: 'english'
+    indexes :keywords, type: :text, analyzer: 'english'
     indexes :published, type: :boolean
   end
 
@@ -313,6 +313,7 @@ class Database < ApplicationRecord
                   'vendor_name^5',
                   'subject_search_index',
                   'curated_search_index',
+                  'keywords',
                   'rss_search_index',
                   'description^2'
                 ],
