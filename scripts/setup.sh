1d5dee8d3a3f64c21f058142a1e041f91a8a140f
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2023-03-06 17:46:26 -0500

    add setup script

---
+++ scripts/setup.sh
@@ -0,0 +1,12 @@
+echo "Preparing Database"
+bin/rails db:drop db:create
+
+# if schema.rb exists load schema else run the migrations
+FILE="/home/databases/db/schema.rb"
+if [ -e $FILE ]; then
+    bin/rails db:schema:load
+else
+    bin/rails db:migrate
+fi
+
+bin/rails db:seed
