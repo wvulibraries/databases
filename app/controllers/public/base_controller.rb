da261cfaa96c7f7cd2512e700f10bc49702911ba
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2023-03-07 13:06:40 -0500

    added method to remove any databases that are not in production from the curated list

--- app/controllers/public/base_controller.rb
+++ app/controllers/public/base_controller.rb
@@ -49,10 +49,28 @@ class Public::BaseController < ApplicationController
   def subject_databases
     subject_id = params[:id]
     @curated = DatabaseCurated.includes(:database).where(subject_id: subject_id).order(sort: :desc)
+    # list only production databases
+    @curated = check_curated_databases(@curated)
     curated_ids = @curated.pluck(:database_id)
     @databases = Database.list_subjects(subject_id).includes(:landing_page).where.not(id: curated_ids).where(status: "production")
     @subject = Subject.find(subject_id)
     @count = @databases.count + @curated.count
     render :subject_db_list
+  end
+
+  # Take an array of ids and check if the database is in production
+  # If they are not in production remove them from the array
+  # @param [Array] ids
+  # @return [Array] ids
+  # @author Tracy A. McCormick
+  def check_curated_databases(curated_items)
+    # loop over each id and check if it is in production
+    curated_items.each do |db|
+      # if not in production remove it from the list
+      if Database.find(db.database_id).status != "production"
+        curated_items.delete(db.id)
+      end
+    end
+    return curated_items
   end
 end
