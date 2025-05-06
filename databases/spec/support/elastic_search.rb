ef01afcb01dcf5ea3594f7c848eac1b7e0ede4b7
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2025-05-05 22:53:38 -0400

    Update elastic_search.rb
    revert update

--- databases/spec/support/elastic_search.rb
+++ databases/spec/support/elastic_search.rb
@@ -1,35 +1,37 @@
-require 'elasticsearch/model'
+# Make sure we're requiring the correct libraries
+require 'elasticsearch'
 
 RSpec.configure do |config|
-  config.before(:each) do
+  config.before(:suite) do
     begin
-      # Make sure we're requiring the right libraries first
-      require 'elasticsearch/model'
+      # For newer elasticsearch gem
+      client = Elasticsearch::Client.new(url: ENV['ELASTICSEARCH_URL'] || 'http://localhost:9200')
       
-      # Create and refresh the index
-      Database.__elasticsearch__.create_index! force: true
-      Database.__elasticsearch__.refresh_index!
-      
-    # Handle different error types depending on gem version
-    rescue Elasticsearch::Transport::Transport::Errors::NotFound => e
-      # This is fine - the index might not exist yet
-    rescue Elasticsearch::Client::NotFound => e
-      # For newer elasticsearch client versions
-    rescue => e
-      STDERR.puts "There was an error creating the elasticsearch index for Database: #{e.inspect}"
-      STDERR.puts e.backtrace.join("\n")
-    end
-  end
-
-  config.after(:each) do
-    begin
-      # Only attempt to delete if the index exists
-      if Database.__elasticsearch__.index_exists?
-        Database.__elasticsearch__.delete_index!
+      # Delete and recreate the index
+      begin
+        client.indices.delete(index: 'databases_test')
+      rescue => e
+        # It's okay if the index doesn't exist yet
+        puts "Note: #{e.message} (this is normal if the index didn't exist)"
       end
+      
+      # Create the index
+      client.indices.create(
+        index: 'databases_test',
+        body: {
+          mappings: {
+            properties: {
+              # Your mappings here
+              title: { type: 'text' },
+              description: { type: 'text' }
+              # Add other fields as needed
+            }
+          }
+        }
+      )
     rescue => e
-      # Just log errors during cleanup, don't fail tests
-      STDERR.puts "Error cleaning up elasticsearch index: #{e.message}"
+      puts "Error setting up Elasticsearch test index: #{e.message}"
+      puts e.backtrace.join("\n")
     end
   end
 end
