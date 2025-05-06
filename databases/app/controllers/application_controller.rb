d045645d860aa2f2f4321686f9f86d5cbf845990
Tracy A. McCormick <tracy.alan.mccormick@gmail.com>
2025-05-06 08:45:32 -0400

    removal of duplicate function

--- databases/app/controllers/application_controller.rb
+++ databases/app/controllers/application_controller.rb
@@ -20,21 +20,6 @@ class ApplicationController < ActionController::Base
     @client_ip ||= IPAddr.new(request.remote_ip)
   end
 
-  # Add this method if it doesn't exist
-  def access_permissions
-    unless authenticated?
-      # Store the original URL in the session so we can redirect back after login
-      session[:return_to] = request.original_url
-      redirect_to login_path
-      return
-    end
-
-    unless admin?
-      flash[:error] = I18n.t('admin.unauthorized')
-      redirect_to root_path
-    end
-  end
-
   def check_session_size
     if Rails.env.development? && session.to_h.to_s.bytesize > 3500
       Rails.logger.warn "Large session detected: #{session.to_h.to_s.bytesize} bytes"
