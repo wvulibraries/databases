2db8e932240328047c01df49988c0639ea8aec29
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2025-05-05 22:40:27 -0400

    Update elastic_search.rb
    testing update

--- databases/spec/support/elastic_search.rb
+++ databases/spec/support/elastic_search.rb
@@ -1,15 +1,37 @@
-RSpec.configure do |config|
-  config.before(:each) do
-    begin
-      Database.__elasticsearch__.create_index!
-      Database.__elasticsearch__.refresh_index!
-    rescue Elasticsearch::Transport::Transport::Errors::NotFound => e
-    rescue => e
-      STDERR.puts "There was an error creating the elasticsearch index for Database: #{e.inspect}"
-    end
-  end
+# Make sure we're requiring the correct libraries
+require 'elasticsearch'
 
-  config.after(:each) do
-    Database.__elasticsearch__.delete_index! if Database.__elasticsearch__.index_exists?
+RSpec.configure do |config|
+  config.before(:suite) do
+    begin
+      # For newer elasticsearch gem
+      client = Elasticsearch::Client.new(url: ENV['ELASTICSEARCH_URL'] || 'http://localhost:9200')
+      
+      # Delete and recreate the index
+      begin
+        client.indices.delete(index: 'databases_test')
+      rescue => e
+        # It's okay if the index doesn't exist yet
+        puts "Note: #{e.message} (this is normal if the index didn't exist)"
+      end
+      
+      # Create the index
+      client.indices.create(
+        index: 'databases_test',
+        body: {
+          mappings: {
+            properties: {
+              # Your mappings here
+              title: { type: 'text' },
+              description: { type: 'text' }
+              # Add other fields as needed
+            }
+          }
+        }
+      )
+    rescue => e
+      puts "Error setting up Elasticsearch test index: #{e.message}"
+      puts e.backtrace.join("\n")
+    end
   end
 end
